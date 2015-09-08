645cf32eb713bc6b56dde7a42981a394017ae04e
ahti.kivi <ahti.kivi@nortal.com>
2015-09-08 20:35:47 +0300

    Cassandara impl

---
+++ dbtest/src/main/java/org/kiva/dbtest/dao/impl/CassandraUserDaoImpl.java
@@ -0,0 +1,32 @@
+package org.kiva.dbtest.dao.impl;
+
+import org.kiva.dbtest.dao.UserDAO;
+import org.kiva.dbtest.model.User;
+
+public class CassandraUserDaoImpl implements UserDAO {
+
+	@Override
+	public User create(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User read(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
