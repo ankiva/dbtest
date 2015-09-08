fbe12900c22c8145586ae6971113fb5be9915e2b
tammurmas <tammurmas@gmail.com>
2015-09-08 20:41:51 +0300

    MongoDAO impl

---
+++ dbtest/src/main/java/org/kiva/dbtest/dao/impl/MongoUserDAO.java
@@ -0,0 +1,34 @@
+package org.kiva.dbtest.dao.impl;
+
+import org.kiva.dbtest.dao.UserDAO;
+import org.kiva.dbtest.model.User;
+
+public class MongoUserDAO implements UserDAO{
+
+	
+	
+	@Override
+	public User create(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User read(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
