33e6666fcab91ae0d7d529d4155267f16699fd07
tammurmas <tammurmas@gmail.com>
2015-09-18 14:03:08 +0300

    HibernateDAO with SessionFactory

--- dbtest/src/main/java/org/kiva/dbtest/dao/impl/HibernateDAO.java
+++ dbtest/src/main/java/org/kiva/dbtest/dao/impl/HibernateDAO.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.ogm.cfg.OgmConfiguration;
 import org.hibernate.service.ServiceRegistry;
 import org.kiva.dbtest.DbTest;
 import org.kiva.dbtest.Utils;
@@ -12,45 +13,41 @@ import org.kiva.dbtest.model.User;
 
 public class HibernateDAO {
 
-	private static final Logger LOG = Logger.getLogger(DbTest.class);
-	
-	public static void main(String[] args) {
-		Utils.confLogger();
-		//Get Session
-        Session session = getSessionFactory().getCurrentSession();
-        //start transaction
-        session.beginTransaction();
-        User emp = DbTest.createUser("test");
-        //Save the Model object
-        session.save(emp);
-        
-        LOG.info(session.contains(emp));
-        
-        session.delete(emp);
-        //Commit transaction
-        session.getTransaction().commit();
+	private static final Logger LOG = Logger.getLogger(HibernateDAO.class);
 
+	public static void main(String[] args) {
+Utils.confLogger();
+		
+		Session session = getSessionFactory("mongodb.cfg.xml").openSession();
+		
+		session.beginTransaction();
+
+		LOG.info("Populating the database...");
+		User usr = DbTest.createUser("fubar");
+
+		session.save(usr);
+		LOG.info("==========================Contains: "+session.contains(usr)+"==========================");
+		
+		session.delete(usr);
+		LOG.info("==========================Contains: "+session.contains(usr)+"==========================");
+		
+		session.getTransaction().commit();
+		session.close();
 	}
-	
-	private static SessionFactory getSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            Configuration configuration = new Configuration();
-            configuration.configure("persistence.cfg.xml");
-            LOG.info("Hibernate Mongo Configuration loaded");
-             
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            LOG.info("Hibernate Mongo serviceRegistry created");
-             
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-             
-            return sessionFactory;
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            LOG.error("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+
+	/**
+	 * 
+	 * @param confName - name of the XML config file in the resources folder
+	 * @return SessionFactory
+	 */
+	private static SessionFactory getSessionFactory(String confName) {
+		Configuration configuration = new OgmConfiguration();
+		configuration.configure(confName);
+		configuration.addAnnotatedClass(org.kiva.dbtest.model.User.class);
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		
+		return configuration.buildSessionFactory(serviceRegistry);
+	}
 
 }
