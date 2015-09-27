9dfd9d4f8578268c127572dfecbe37daac556449
tammurmas <tammurmas@gmail.com>
2015-09-27 21:22:44 +0300

    HibernateDAO

---
+++ dbtest/src/main/java/org/kiva/dbtest/dao/impl/HibDAOTest.java
@@ -0,0 +1,47 @@
+package org.kiva.dbtest.dao.impl;
+
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+import org.kiva.dbtest.DbTest;
+import org.kiva.dbtest.DbType;
+import org.kiva.dbtest.Utils;
+import org.kiva.dbtest.model.User;
+
+public class HibDAOTest {
+	private static final Logger LOG = Logger.getLogger(DbTest.class);
+	
+	public static void main(String[] args) {
+		Utils.confLogger();
+		
+		HibernateDAO hibDAO = new HibernateDAO(DbType.parse(args[0]));
+		hibDAO.init();
+		
+		Iterator<?> it = hibDAO.getAll().iterator();
+		System.out.println("===Existing users:===");
+		while(it.hasNext())
+		{
+			System.out.println();
+			System.out.println(it.next());
+			System.out.println();
+		}
+		
+		User u = DbTest.createUser("fuubar");
+		hibDAO.create(u);
+		User fuubar = hibDAO.read(u.getUserName());
+		assert fuubar != null;
+		LOG.info("===Created user: "+u+"===");
+		
+		fuubar.setLastName("Vsevolod-Hui");
+		hibDAO.update(fuubar);
+		User updatedUser = hibDAO.read(u.getUserName());
+		assert updatedUser.getLastName().equals("Vsevolod-Hui");
+		LOG.info("===Updated user: "+updatedUser+"===");
+		
+		hibDAO.delete(updatedUser);
+		assert hibDAO.read("fuubar") == null;
+		LOG.info("===Deleted user: "+updatedUser+"===");
+		
+		hibDAO.destroy();
+	}
+}
--- dbtest/src/main/java/org/kiva/dbtest/dao/impl/HibernateDAO.java
+++ dbtest/src/main/java/org/kiva/dbtest/dao/impl/HibernateDAO.java
@@ -1,56 +1,140 @@
 package org.kiva.dbtest.dao.impl;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.ogm.cfg.OgmConfiguration;
 import org.hibernate.service.ServiceRegistry;
 import org.kiva.dbtest.DbTest;
-import org.kiva.dbtest.Utils;
+import org.kiva.dbtest.DbType;
+import org.kiva.dbtest.dao.UserDAO;
 import org.kiva.dbtest.model.User;
 
-public class HibernateDAO {
+public class HibernateDAO implements UserDAO {
 
 	private static final Logger LOG = Logger.getLogger(DbTest.class);
-	
-	public static void main(String[] args) {
-		Utils.confLogger();
-		//Get Session
-        Session session = getSessionFactory().getCurrentSession();
-        //start transaction
-        session.beginTransaction();
-        User emp = DbTest.createUser("test");
-        //Save the Model object
-        session.save(emp);
-        
-        LOG.info(session.contains(emp));
-        
-        session.delete(emp);
-        //Commit transaction
-        session.getTransaction().commit();
+	private SessionFactory sessionFactory;
+	private DbType dbType;
 
+	public HibernateDAO(DbType dbType) {
+		this.dbType = dbType;
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * Create a session factory from the cfg.xml file
+	 * 
+	 * @param dbType - enum
+	 */
+	private void setSessionFactory(DbType dbType) {
+		Configuration configuration = new OgmConfiguration();
+		
+		switch (dbType) {
+		case CASSANDRA:
+			// TODO: create cassandra.cfg.xml
+			configuration.configure("cassandra.cfg.xml");
+			LOG.info("Hibernate Cassandra Configuration loaded");
+			break;
+		case MONGODB:
+			configuration.configure("mongodb.cfg.xml");
+			LOG.info("Hibernate MongoDB Configuration loaded");
+			break;
+		case REDIS:
+			// TODO: create redis.cfg.xml
+			configuration.configure("redis.cfg.xml");
+			LOG.info("Hibernate Redis Configuration loaded");
+			break;
+		}
+
+		configuration.addAnnotatedClass(org.kiva.dbtest.model.User.class);
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		LOG.info("Hibernate ServiceRegistry created");
+
+		SessionFactory sessFactory = configuration.buildSessionFactory(serviceRegistry);
+
+		sessionFactory = sessFactory;
 	}
 	
-	private static SessionFactory getSessionFactory() {
-        try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            Configuration configuration = new Configuration();
-            configuration.configure("persistence.cfg.xml");
-            LOG.info("Hibernate Mongo Configuration loaded");
-             
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            LOG.info("Hibernate Mongo serviceRegistry created");
-             
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-             
-            return sessionFactory;
-        }
-        catch (Throwable ex) {
-            // Make sure you log the exception, as it might be swallowed
-            LOG.error("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+	public User create(User user)
+	{
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		if(!session.contains(user))
+		{
+			session.save(user);
+		}
+		User loadedUser = session.get(User.class, user.getUserName());
+		transaction.commit();
+		return loadedUser;
+	}
+	
+	public void delete(User user)
+	{
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		if(!session.contains(user))
+		{
+			session.delete(user);
+		}
+		transaction.commit();
+	}
+	
+	public User read(String userName)
+	{
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		User loadedUser = session.get(User.class, userName);
+		transaction.commit();
+		return loadedUser;
+	}
+	
+	public void update(User user)
+	{
+		Session session = sessionFactory.getCurrentSession();
+		Transaction transaction = session.beginTransaction();
+		session.saveOrUpdate(user);
+		transaction.commit();
+	}
 
+	@Override
+	public void init() {
+		setSessionFactory(dbType);
+	}
+
+	@Override
+	public void destroy() {
+		sessionFactory.close();
+	}
+
+	@Override
+	public void deleteAll() {
+		Iterator<?> it = getAll().iterator();
+		Session session = sessionFactory.getCurrentSession();
+		
+		Transaction transaction = session.beginTransaction();
+		while(it.hasNext())
+		{
+			delete((User)it.next());
+		}
+		transaction.commit();
+	}
+	
+	public List<?> getAll()
+	{
+		Session session = sessionFactory.getCurrentSession();
+		Transaction trans = session.beginTransaction();
+		List<?> users = session.createQuery("SELECT u FROM User u").list();
+		trans.commit();
+		return users;
+	}
 }
